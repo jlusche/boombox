2a5827d6a9b2c50950932a65338658eac1778a30
Jenna Lusche <jennalusche@gmail.com>
2014-11-16 19:08:45 -0800

    bands with index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController    #classes have to be capitalized
   def home
   end
+
   def result 
-    @artist=params['artist']
+    artist=params['artist']
+    url_safe_artist = artist.gsub(' ', '+')
+    echo_result= HTTParty.get"http://developer.echonest.com/api/v4/artist/search?api_key=Q8MXVVC4CMXZEIHEG&name=#{url_safe_artist}"
+    @all_artists= echo_result['response']['artists']
+   
   end
 end
